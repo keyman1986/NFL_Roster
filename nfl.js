551d56902c94c2889fd9b31c2227c1a48d2dc567
Brandon Ashcraft <keyman1986@gmail.com>
2015-09-16 10:10:41 -0600

    ready for jquery

--- nfl.js
+++ nfl.js
@@ -1,1 +1,25 @@
-{\rtf1}
+
+
+
+
+
+
+
+var roster;
+
+function addToRoster(a,b,c){
+	this.name = a;
+	this.position = b;
+	this.number = c;
+	return roster;
+}
+
+
+$("#addPlayerButton").on('click',function(e){
+	var name = "Test Name";
+	var position = "Test Position";
+	var number = "Test Number"; 
+	
+	$(".roster").append("Does this work?");
+	e.preventDefault();
+});
