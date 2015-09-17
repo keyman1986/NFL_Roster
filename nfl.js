1fbe5b13df0d6f9cbe2f0c8ef24e7f97957992ee
Brandon Ashcraft <keyman1986@gmail.com>
2015-09-16 21:02:59 -0600

    changes

--- nfl.js
+++ nfl.js
@@ -1,25 +1,24 @@
 
+var position = document.getElementById("positions"); 
+var options = ["Front", "Center", "Middle", "Ball Holder", "Water Boy"]; 
 
-
-
-
-
-
-var roster;
-
-function addToRoster(a,b,c){
-	this.name = a;
-	this.position = b;
-	this.number = c;
-	return roster;
-}
-
+for(var i = 0; i < options.length; i++) {
+    var opt = options[i];
+    var elem = document.createElement("option");
+    elem.textContent = opt;
+    elem.value = opt;
+    position.appendChild(elem);
+};
 
 $("#addPlayerButton").on('click',function(e){
-	var name = "Test Name";
-	var position = "Test Position";
-	var number = "Test Number"; 
-	
-	$(".roster").append("Does this work?");
 	e.preventDefault();
+	$(".roster").append('<div class="center player-card"><button class="btn btn-danger remove-button">Remove</button></span></br><span><img src="player.png" alt="Person 1"></span></br><span>Name</span></br><span>Position</span></br><span>Number</div></br>');
+});
+
+
+
+$('.roster').on('click', '.remove-button', function(e){
+	e.preventDefault();
+	$(this).parent().remove()
+	
 });
